ac2cf87307ae6eb54982a62240c51eb9bc2f9300
Alexandr Fisenko <80264183+Alfi2490@users.noreply.github.com>
2021-10-24 11:49:28 +0300

    commit

--- showFavorites.js
+++ showFavorites.js
@@ -1,9 +1,36 @@
 function showFavorites() {
+
     films = getFavoriteFilms();
+
     showFavoritesImplementation();
+
 };
 
 function showFavoritesImplementation() {
-    renderFilmsList();
+
+    let filmsList = document.querySelector(".FilmsList");
+    filmsList.innerHTML = '';
+
+    let listOfFilms = '';
+
+    films.forEach(f => {        
+        listOfFilms = listOfFilms + `
+            <div class="FilmFavorite" id="${f.id}">
+                <img class="FilmPosterFavorite" src="${IMG_URL}${f.poster_path}" >
+                <div>
+                    <h1>${f.original_title}</h1>
+                    <h2>${f.release_date}</h2>
+                </div>
+                <div class="AdditionalInformationInFavs">
+                    <h1>Original language: ${f.original_language}</h1>
+                    <h1>Popularity: ${f.popularity}</h1>
+                </div>
+                <button onclick="removeFromFavorites(event, parentElement.id, 'clickFromFavorites')">Remove from favorites</button>
+            </div>
+        `;
+
+    });
+
+    filmsList.innerHTML = listOfFilms;
     activeMenuPosition = 'favorites';
 }
