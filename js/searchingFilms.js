ff060c571d210b6659c80f10286b377d1607a312
Alexandr Fisenko <80264183+Alfi2490@users.noreply.github.com>
2021-11-01 15:05:22 +0200

    async search complete

---
+++ js/searchingFilms.js
@@ -0,0 +1,33 @@
+const titleInput = document.getElementById('Title');
+titleInput.addEventListener('input', updateTitle);
+let searchTimeOut;
+
+let title = '';
+
+function updateTitle(e) {
+
+    clearTimeout(searchTimeOut);    
+    title = e.target.value.trim().toLowerCase();
+    if(title.length <= 2) {
+        return
+    } else {
+        searchTimeOut = setTimeout(searchingFilms, 1000);
+    }    
+
+}
+
+function searchingFilms() {    
+
+    getData('search', title).then(res => {
+        films = res.results;
+        searchFilmsImplemetation();
+    }).catch(err => alert(err));
+
+};
+
+function searchFilmsImplemetation() {
+
+    renderFilmsList();
+    activeMenuPosition = 'search';
+
+};
